1793529e344accb9240dc2482552491d4668fc73
swetha-sankar <swetha@udel.edu>
2021-06-28 18:36:28 -0400

    changes to style, lyric functionality

--- src/App.js
+++ src/App.js
@@ -1,135 +1,107 @@
-import React, { Component } from "react";
+import React, {Component, useEffect, useState} from 'react';
 import * as $ from "jquery";
-import { authEndpoint, clientId, redirectUri, scopes } from "./config";
+import {authEndpoint, clientId, redirectUri, scopes} from "./config";
 import hash from "./hash";
-import Player from "./components/Player";
+import CurrentSong from "./components/CurrentSong";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      token: null,
-      item: {
-        album: {
-          images: [{ url: "" }]
-        },
-        name: "",
-        artists: [{ name: "" }],
-      },
-      is_playing: "Paused",
-      no_data: false,
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            token: null,
+            item: {
+                album: {
+                    images: [{url: ""}]
+                },
+                name: "",
+                artists: [{name: ""}],
+            },
+            is_playing: "Paused",
+            no_data: false,
+        };
 
-    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
-    this.tick = this.tick.bind(this);
-  }
-
-
-
-  componentDidMount() {
-    // Set token
-    let _token = hash.access_token;
-
-    if (_token) {
-      // Set token
-      this.setState({
-        token: _token
-      });
-      this.getCurrentlyPlaying(_token);
+        this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+        this.tick = this.tick.bind(this);
     }
 
-    // set interval for polling every 5 seconds
-    this.interval = setInterval(() => this.tick(), 5000);
-  }
+    componentDidMount() {
+        let _token = hash.access_token;
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  tick() {
-    if(this.state.token) {
-      this.getCurrentlyPlaying(this.state.token);
+        if (_token) {
+            this.setState({
+                token: _token
+            });
+            this.getCurrentlyPlaying(_token);
+        }
+        this.interval = setInterval(() => this.tick(), 5000);
     }
-  }
 
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
 
-  getCurrentlyPlaying(token) {
-    $.ajax({
-      url: "https://api.spotify.com/v1/me/player",
-      type: "GET",
-      beforeSend: xhr => {
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
-      },
-      success: data => {
-        if(!data) {
-          this.setState({
-            no_data: true,
-          });
-          return;
+    tick() {
+        if (this.state.token) {
+            this.getCurrentlyPlaying(this.state.token);
         }
-        this.setState({
-          item: data.item,
-          is_playing: data.is_playing,
-          no_data: false
+    }
+
+
+    getCurrentlyPlaying(token) {
+        $.ajax({
+            url: "https://api.spotify.com/v1/me/player",
+            type: "GET",
+            beforeSend: xhr => {
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
+            },
+            success: data => {
+                if (!data) {
+                    this.setState({
+                        no_data: true,
+                    });
+                    return;
+                }
+                this.setState({
+                    item: data.item,
+                    is_playing: data.is_playing,
+                    no_data: false
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
-  getCurrentLyrics(songTitle, artistName){
-     $.ajax({
-      url: "https://api.genius.com",
-      type: "GET",
-      beforeSend: xhr => {
-        xhr.setRequestHeader("Authorization", "Bearer ", "nACQIYh6vT2awT4z2kKyl3X7lnZeBvJdpiADEGF6AHqBBfXhsuz8ReBTIC2z6iTZ");
-      },
-      success: data => {
-        if(!data) {
-          this.setState({
-            no_data: true,
-          });
-          return;
-        }
-        this.setState({
-          item: data.item,
-          is_playing: data.is_playing,
-          no_data: false
-        });
-      }
-    });
 
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {!this.state.token && (
+                        <a
+                            className="btn btn--loginApp-link"
+                            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                                "%20"
+                            )}&response_type=token&show_dialog=true`}
+                        >
+                            First, Log In to Spotify
+                        </a>
+                    )}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          {!this.state.token && (
-            <a
-              className="btn btn--loginApp-link"
-              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                "%20"
-              )}&response_type=token&show_dialog=true`}
-            >
-              First, Log In to Spotify
-            </a>
-          )}
+                    {this.state.token && !this.state.no_data && (
+                        <CurrentSong
+                            item={this.state.item}
+                            is_playing={this.state.is_playing}
+                        />
+                        ) }
 
-          {this.state.token && !this.state.no_data && (
-            <Player
-              item={this.state.item}
-              is_playing={this.state.is_playing}
-            />
-          )}
-          {this.state.no_data && (
-            <p>
-              Nothing currently playing
-            </p>
-          )}
-        </header>
-      </div>
-    );
-  }
+                    {this.state.no_data && (
+                        <p>
+                            Nothing currently playing
+                        </p>
+                    )}
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
